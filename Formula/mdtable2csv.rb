c345442af82cca41a78e0f36cae64fe71e8d82bb
515hikaru <12kojima.takahiro@gmail.com>
2020-02-22 10:38:07 +0900

    fix syntax error

--- Formula/mdtable2csv.rb
+++ Formula/mdtable2csv.rb
@@ -1,7 +1,7 @@
 class Mdtable2csv < Formula
   desc "Convert markdown table to csv"
   homepage "https://github.com/515hikaru/mdtable2csv"
-  url "https://github.com/515hikaru/mdtable2csv.git"
+  url "https://github.com/515hikaru/mdtable2csv.git",
       :tag      => "v1.0.0",
       :revision => "ee0ac3eeb750446f5b4f680386649409bca4188b"
   head "https://github.com/515hikaru/mdtable2csv.git"
