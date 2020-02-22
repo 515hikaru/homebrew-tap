384e22023ed7755ca9b416430db55f94495fb906
515hikaru <12kojima.takahiro@gmail.com>
2020-02-22 10:36:17 +0900

    try sample formula

---
+++ Formula/mdtable2csv.rb
@@ -0,0 +1,19 @@
+class Mdtable2csv < Formula
+  desc "Convert markdown table to csv"
+  homepage "https://github.com/515hikaru/mdtable2csv"
+  url "https://github.com/515hikaru/mdtable2csv.git"
+      :tag      => "v1.0.0",
+      :revision => "ee0ac3eeb750446f5b4f680386649409bca4188b"
+  head "https://github.com/515hikaru/mdtable2csv.git"
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    system "false"
+  end
+end
