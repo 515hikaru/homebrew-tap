d5b7cea5a7f85a9ac65ddd4e0609ec5d853d6f7f
Takahiro KOJIMA <12kojima.takahiro@gmail.com>
2022-09-03 12:54:14 +0900

    update formula

--- Formula/mdtable2csv.rb
+++ Formula/mdtable2csv.rb
@@ -1,10 +1,9 @@
 class Mdtable2csv < Formula
   desc "Convert markdown table to csv"
   homepage "https://github.com/515hikaru/mdtable2csv"
-  url "https://github.com/515hikaru/mdtable2csv.git",
-      :tag      => "v1.0.0",
-      :revision => "ee0ac3eeb750446f5b4f680386649409bca4188b"
-  head "https://github.com/515hikaru/mdtable2csv.git"
+  url "https://github.com/515hikaru/mdtable2csv/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "73ba7503d68236cde6c9eba52d9e15dfaf4b2eb12ca3b1404c8fd74b6256d81e"
+  license "MIT"
 
   depends_on "go" => :build
 
